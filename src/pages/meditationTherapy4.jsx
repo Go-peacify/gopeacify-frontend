657d76a7b00cdeb005de11d29a8eb7977f2c862a
Aakash <aakashskilldevelopment@gmail.com>
2023-05-12 19:31:33 +0530

    Meditation page added Routing/Responsive done

--- src/pages/meditationTherapy4.jsx
+++ src/pages/meditationTherapy4.jsx
@@ -11,49 +11,49 @@ import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 const meditationTherapy4 = () => {
     return (
         <div className="MeditationTherapy4 TranscedentalMeditation">
-        <div className="med4 trans">
-            <div className="TranscedentalMed">
-                <p>Transcedental Meditation</p>
+            <div className="med4 trans">
+                <div className="TranscedentalMed">
+                    <p>Transcedental Meditation</p>
+                </div>
+                <div className="blogImgPart">
+                    <div className="transLayer2 tr2">
+                        <img src={rect40019} alt="" srcset="" />
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo quae repudiandae perferendis consectetur, eligendi ipsa a tempora alias ipsam? Harum unde eum sed odit sit modi voluptatum expedita eos recusandae quo a id suscipit labore cupiditate eveniet quibusdam, consequuntur quod? Sequi hic deleniti eaque dolorem voluptate nam dicta cumque ipsa quae amet, dolore ipsam saepe, maiores, aut natus modi qui! Facilis quo reprehenderit nemo ipsa blanditiis nostrum molestiae voluptas maiores beatae? Pariatur nostrum veniam ad consequuntur in magnam sapiente minima deserunt consequatur unde, dignissimos animi, velit inventore dolor. Consequuntur, adipisci amet! Ad fugiat nesciunt maxime! Amet recusandae voluptas accusamus minus.</p>
+                    </div>
+                    <div className="transLayer2">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo quae repudiandae perferendis consectetur, eligendi ipsa a tempora alias ipsam? Harum unde eum sed odit sit modi voluptatum expedita eos recusandae quo a id suscipit labore cupiditate eveniet quibusdam, consequuntur quod? Sequi hic deleniti eaque dolorem voluptate nam dicta cumque ipsa quae amet, dolore ipsam saepe, maiores, aut natus modi qui! Facilis quo reprehenderit nemo ipsa blanditiis nostrum molestiae voluptas maiores beatae? Pariatur nostrum veniam ad consequuntur in magnam sapiente minima deserunt consequatur unde, dignissimos animi, velit inventore dolor. Consequuntur, adipisci amet! Ad fugiat nesciunt maxime! Amet recusandae voluptas accusamus minus.</p>
+                        <img src={l1} alt="" srcset="" />
+                        <img src={l2} alt="" srcset="" />
+                    </div>
+                </div>
             </div>
-            <div className="blogImgPart">
-                <div className="transLayer2 tr2">
-                    <img src={rect40019} alt="" srcset="" />
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo quae repudiandae perferendis consectetur, eligendi ipsa a tempora alias ipsam? Harum unde eum sed odit sit modi voluptatum expedita eos recusandae quo a id suscipit labore cupiditate eveniet quibusdam, consequuntur quod? Sequi hic deleniti eaque dolorem voluptate nam dicta cumque ipsa quae amet, dolore ipsam saepe, maiores, aut natus modi qui! Facilis quo reprehenderit nemo ipsa blanditiis nostrum molestiae voluptas maiores beatae? Pariatur nostrum veniam ad consequuntur in magnam sapiente minima deserunt consequatur unde, dignissimos animi, velit inventore dolor. Consequuntur, adipisci amet! Ad fugiat nesciunt maxime! Amet recusandae voluptas accusamus minus.</p>
-                </div>
-                <div className="transLayer2">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo quae repudiandae perferendis consectetur, eligendi ipsa a tempora alias ipsam? Harum unde eum sed odit sit modi voluptatum expedita eos recusandae quo a id suscipit labore cupiditate eveniet quibusdam, consequuntur quod? Sequi hic deleniti eaque dolorem voluptate nam dicta cumque ipsa quae amet, dolore ipsam saepe, maiores, aut natus modi qui! Facilis quo reprehenderit nemo ipsa blanditiis nostrum molestiae voluptas maiores beatae? Pariatur nostrum veniam ad consequuntur in magnam sapiente minima deserunt consequatur unde, dignissimos animi, velit inventore dolor. Consequuntur, adipisci amet! Ad fugiat nesciunt maxime! Amet recusandae voluptas accusamus minus.</p>
-                    <img src={l1} alt="" srcset="" />
-                    <img src={l2} alt="" srcset="" />
-                </div>
-            </div>
-        </div>
-        <div className="fourthLayer y">
-            <div className="flstart">
-                <div className="b">
-                    <span>BLOG</span>
-                </div>
-                <div className="fl1 b">
-                    <p>You cannot always control what goes on outside, but you can always control what goes on inside.</p>
-                </div>
-                <div className='flmoveBtn'>
-                    <div className="fl2 b ">
-                        <ArrowBackIosIcon />
-                        <ArrowForwardIosIcon />
+            <div className="fourthLayer y">
+                <div className="flstart">
+                    <div className="b">
+                        <span>BLOG</span>
                     </div>
-                </div>
-                <div className="fl3 b">
-                    <div>
-                        <img src={newrec1} alt="" srcset="" />
-                        <span></span>
+                    <div className="fl1 b">
+                        <p>You cannot always control what goes on outside, but you can always control what goes on inside.</p>
                     </div>
-                    <div>
-                        <img src={newrec2} alt="" srcset="" />
-                        <span></span>
+                    <div className='flmoveBtn'>
+                        <div className="fl2 b ">
+                            <ArrowBackIosIcon />
+                            <ArrowForwardIosIcon />
+                        </div>
                     </div>
-                    <div>
-                        <img src={newrec3} alt="" srcset="" />
-                        <span></span>
-                    </div>
+                    <div className="fl3 b">
+                        <div>
+                            <img src={newrec1} alt="" srcset="" />
+                            <span></span>
+                        </div>
+                        <div>
+                            <img src={newrec2} alt="" srcset="" />
+                            <span></span>
+                        </div>
+                        <div>
+                            <img src={newrec3} alt="" srcset="" />
+                            <span></span>
+                        </div>
 
                 </div>
 
